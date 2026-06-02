066ffa830cef7a84a5cdda2749aaa61f65db8e24
Rishabh-Bisht <workrishabh2005@gmail.com>
2026-06-02 13:05:55 +0530

    Update backend server

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.use(
     },
     credentials: true, // Allow cookies
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma"],
   })
 );
 
